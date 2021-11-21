4052067caa467bf1fd9887e7f3aee415f70e329b
Germán Tano <germantano88@gmail.com>
2021-11-21 19:51:51 -0300

    Extraído header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FooterComponent } from './footer/footer.component';
